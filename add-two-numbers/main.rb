22ee978cce648526ab49001da5f6ff984cbd6767
akahige <akahigeg@gmail.com>
2022-11-25 18:33:54 +0900

    ディスカッションを参考にAdd Two Numbersを一桁ずつ足していくアプローチで こちらの方が速い

--- add-two-numbers/main.rb
+++ add-two-numbers/main.rb
@@ -10,10 +10,29 @@
 # @param {ListNode} l2
 # @return {ListNode}
 def add_two_numbers(l1, l2)
-   n1 = get_linked_number(l1).reverse.to_i
-   n2 = get_linked_number(l2).reverse.to_i
+  add_two_numbers_by_digit(l1, l2, 0)
 
-   (n1 + n2).to_s.split(//).map(&:to_i).reverse
+  # 数値にして足して、それからまた裏返すアプローチ 遅いが分かりやすさがある
+  # n1 = get_linked_number(l1).reverse.to_i
+  # n2 = get_linked_number(l2).reverse.to_i
+  # (n1 + n2).to_s.split(//).map(&:to_i).reverse
+end
+
+def add_two_numbers_by_digit(node1, node2, moveup)
+  if node1.nil? && node2.nil?
+    return moveup == 0 ? nil : ListNode.new(moveup)
+  end
+
+  digit_sum = (node1&.val || 0) + (node2&.val || 0) + moveup
+
+  if digit_sum >= 10
+    moveup = 1
+    digit_sum -= 10
+  else
+    moveup = 0
+  end
+
+  ListNode.new(digit_sum, add_two_numbers_by_digit(node1&.next, node2&.next, moveup))
 end
 
 def get_linked_number(listnode, number = "")
