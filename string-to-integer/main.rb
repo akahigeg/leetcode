2a30388a3bd1be81c86a2e1b73d07279297a10ae
akahige <akahigeg@gmail.com>
2022-11-28 21:07:01 +0900

    Rubyだとatoiがほぼto_iで終わることに気づいた

--- string-to-integer/main.rb
+++ string-to-integer/main.rb
@@ -20,6 +20,15 @@ def my_atoi(s)
   number
 end
 
+# to_iだけでほぼいけるって言う
+# ref: https://leetcode.com/problems/string-to-integer-atoi/discuss/1604084/ruby-solution
+def my_atoi_only_to_i(s)
+  max = (2**31) - 1
+  min = (-2**31)
+  return s.to_i if s.to_i <= max && s.to_i >= min
+  s.to_i < min ? min : max
+end
+
 p my_atoi(" -42")
 p my_atoi("42")
 p my_atoi("404 not found")
