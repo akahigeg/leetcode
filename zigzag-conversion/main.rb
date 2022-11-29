ac6039846a1a004b5324c7aa8a81047938733067
akahige <akahigeg@gmail.com>
2022-11-29 10:41:04 +0900

    Ruby 6. Zigzag Conversion 競プロっぽくeachなどを廃した版

--- zigzag-conversion/main.rb
+++ zigzag-conversion/main.rb
@@ -2,9 +2,47 @@
 # @param {Integer} num_rows
 # @return {String}
 
+# Runtime: 267 ms, faster than 61.54% of Ruby online submissions for Zigzag Conversion.
+#Memory Usage: 211.2 MB, less than 50.00% of Ruby online submissions for Zigzag Conversion.
+def convert(s, num_rows)
+  return s if num_rows == 1
+
+  s_length = s.size
+
+  lines = []
+  num_rows.times { lines << [] }
+
+  last_line_index = num_rows - 1
+
+  i = 0
+  while i < s_length
+    # 縦
+    line_index = 0
+    while line_index < num_rows
+      lines[line_index] << s[i] unless s[i].nil?
+      line_index += 1
+      i += 1
+    end
+
+    # 斜め
+    line_index = last_line_index - 1
+    while line_index > 0
+      lines[line_index] << s[i] unless s[i].nil?
+      line_index -= 1
+      i += 1
+    end
+  end
+
+  lines.join
+end
+
+# 実行時間は誤差
+#
 # Runtime: 115 ms, faster than 98.46% of Ruby online submissions for Zigzag Conversion.
 # Memory Usage: 211 MB, less than 76.92% of Ruby online submissions for Zigzag Conversion.
-def convert(s, num_rows)
+# Runtime: 274 ms, faster than 58.46% of Ruby online submissions for Zigzag Conversion.
+# Memory Usage: 211.3 MB, less than 50.00% of Ruby online submissions for Zigzag Conversion.
+def convert_easy_implimantation(s, num_rows)
   chars = s.chars
 
   lines = []
