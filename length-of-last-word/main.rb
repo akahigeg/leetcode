49515e4c48e0e3a1ac7815fb4c600a5536f60f50
akahige <akahigeg@gmail.com>
2022-12-04 11:07:50 +0900

    Ruby 58. Length of Last Word

---
+++ length-of-last-word/main.rb
@@ -0,0 +1,13 @@
+# @param {String} s
+# @return {Integer}
+
+# Runtime: 84 ms, faster than 92.45% of Ruby online submissions for Length of Last Word.
+# Memory Usage: 210.9 MB, less than 83.40% of Ruby online submissions for Length of Last Word.
+def length_of_last_word(s)
+  s.strip.split(" ").last.size
+end
+
+p length_of_last_word("Hello world")
+p length_of_last_word("  fly me  to  the moon  ")
+p length_of_last_word("luffy is still joyboy")
+
