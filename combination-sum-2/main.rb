59e9b8882011b91ead3e9b7b79b83056a00cc108
akahige <akahigeg@gmail.com>
2022-12-05 14:23:11 +0900

    Ruby 40. Combination Sum II よく分からないなりにコメントを追加

--- combination-sum-2/main.rb
+++ combination-sum-2/main.rb
@@ -9,23 +9,27 @@
 def combination_sum2(candidates, target)
   candidates.sort!
   
-  generate_combination_sum(candidates, target, 0)
+  # find_combinations(candidates, target, 0).map{|c| c.sort }.uniq 最後にuniqは時間切れになる
+  find_combinations(candidates, target, 0)
 end
   
-def generate_combination_sum(candidates, target, combinations = [], combination = [], start)
+def find_combinations(candidates, target, combinations = [], combination = [], start)
   if target == 0
     combinations << combination.clone 
     return
   end
-    
-  # return if candidates[start].nil? || target < candidates[start]
   
   (start...candidates.length).each do |i|
-    next if candidates[i] == candidates[i-1] && i > start
-    break if candidates[i] > target
+    n = candidates[i]
+
+    # 同じ数の連続があった場合、先頭のひとつだけを取る。これがなんで重複の解決策になるのかよく分からん
+    # => 再帰が進めばいずれ足し算されていくので、スキップするということ？意外とアドホックなやり方なのかも
+    next if n == candidates[i-1] && i > start 
+
+    break if n > target
     
-    combination << candidates[i]
-    generate_combination_sum(candidates, target - candidates[i], combinations, combination, i+1)
+    combination << n
+    find_combinations(candidates, target - n, combinations, combination, i+1)
     
     combination.pop
   end
@@ -34,5 +38,5 @@ def generate_combination_sum(candidates, target, combinations = [], combination 
 end
 
 # p combination_sum2([1,2,1,5], 8)
-p combination_sum2([10,1,2,7,6,1,5], 8)
+p combination_sum2([10,1,2,7,6,1,5, 1], 8)
 p combination_sum2([2,5,2,1,2], 5)
