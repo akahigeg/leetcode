737beeea61457bc4d91899d798cdb5fbb5b32cdd
akahige <akahigeg@gmail.com>
2022-12-09 18:25:48 +0900

    Ruby 53. Maximum Subarray Kadane's algorithm

--- maximum-subarray/main.rb
+++ maximum-subarray/main.rb
@@ -1,6 +1,19 @@
 # @param {Integer[]} nums
 # @return {Integer}
-def max_sub_array_bf(nums)
+
+# Runtime 190 ms Beats 80.67%
+# Memory 220.4 MB Beats 15.13%
+def max_sub_array(nums)
+  current_subarray = max_subarray = nums[0]
+  nums[1...nums.size].each do |n|
+    current_subarray = [n, current_subarray + n].max
+    max_subarray = [max_subarray, current_subarray].max
+  end
+
+  max_subarray
+end
+
+def max_sub_array_obf(nums)
   max_subarray = -Float::INFINITY
   nums.each_with_index do |n, i|
     current_sub_array = 0
@@ -14,5 +27,5 @@ def max_sub_array_bf(nums)
 end
 
 p max_sub_array([-2, 1, -3, 4, -1, 2, 1, -5, 4])
-# p max_sub_array([1])
-# p max_sub_array([5,4,-1,7,8])
+p max_sub_array([1])
+p max_sub_array([5, 4, -1, 7, 8])
