336dbd2796c348b1521c1fe4c34975fdb65a250f
akahige <akahigeg@gmail.com>
2022-12-13 12:47:02 +0900

    Ruby 82. Remove Duplicates from Sorted List II 途中まで実装

---
+++ lib/ruby-linked-list.rb
@@ -0,0 +1,40 @@
+class ListNode
+  attr_accessor :val, :next
+
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+
+  def to_array
+    array = []
+    current = self
+    while current
+      array << current.val
+      current = current.next
+    end
+
+    array
+  end
+end
+
+class Array
+  def to_linked_list(is_cycle = false)
+    head = nil
+    prev = nil
+    current = nil
+    self.each do |n|
+      current = ListNode.new(n)
+      head ||= current
+      prev.next = current if prev
+
+      prev = current
+    end
+
+    if is_cycle
+      current.next = head
+    else
+      head
+    end
+  end
+end
