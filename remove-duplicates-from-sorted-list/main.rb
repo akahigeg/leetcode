3b55428ffc973a71a14f77582ae4f22209da4c1e
akahige <akahigeg@gmail.com>
2022-12-14 18:30:19 +0900

    Ruby 83. Remove Duplicates from Sorted List

---
+++ remove-duplicates-from-sorted-list/main.rb
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+
+require_relative "../lib/ruby-linked-list"
+
+# Runtime 88 ms Beats 89.41%
+# Memory 210.9 MB Beats 88.24%
+def delete_duplicates(head)
+  current = head
+
+  while current && current.next
+    # 値が違えばcurrentを移動して次へ
+    if current.val != current.next.val
+      current = current.next
+      next
+    end
+
+    # 値が同じならcurrentはそのままでnextをひとつ飛ばす
+    current.next = current.next.next
+  end
+
+  head
+end
+
+p delete_duplicates([1, 1, 2].to_linked_list).to_array
+p delete_duplicates([1, 1, 2, 3, 3, 4, 4, 5].to_linked_list).to_array
