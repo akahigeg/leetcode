420a6f3500c33fd5b5bda51a1a97c889d4fd358a
akahige <akahigeg@gmail.com>
2022-12-15 13:00:20 +0900

    Ruby 102. Binary Tree Level Order Traversal キューを使った幅優先探索

--- binary-tree-level-order-traversal/main.rb
+++ binary-tree-level-order-traversal/main.rb
@@ -12,9 +12,31 @@
 
 require_relative "../lib/ruby-tree-node"
 
+# ref: https://leetcode.com/problems/binary-tree-level-order-traversal/solutions/2283024/bfs-in-ruby/
+# Runtime 92 ms Beats 86.70%
+# Memory 211.2 MB Beats 16.75%
+def level_order(root)
+  return [] if root.nil?
+
+  levels = []
+  queue = [root]
+
+  until queue.empty?
+    levels << queue.map { |node| node.val }
+
+    queue.size.times do
+      node = queue.shift
+      queue << node.left if node.left
+      queue << node.right if node.right
+    end
+  end
+
+  levels
+end
+
 # Runtime 88 ms Beats 90.15%
 # Memory 211.1 MB Beats 16.75%
-def level_order(root)
+def level_order_recurse(root)
   levels = []
   traversal(root, levels)
 
