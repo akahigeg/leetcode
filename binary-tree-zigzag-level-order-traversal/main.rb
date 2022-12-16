1f1365093a361127092c934c514f3b5c932d1988
akahige <akahigeg@gmail.com>
2022-12-16 15:51:30 +0900

    Ruby 103. Binary Tree Zigzag Level Order Traversal リファクタして高速化

--- binary-tree-zigzag-level-order-traversal/main.rb
+++ binary-tree-zigzag-level-order-traversal/main.rb
@@ -12,9 +12,42 @@
 
 require_relative "../lib/ruby-tree-node"
 
+# 各レベルにnode.valを突っ込むタイミングと方法を修正して高速化
+#   ループの回数を減らした（mapをなくした）
+#   レベルのnode.valを最初から逆順で入れるようにした（あとでreverseするのをやめた）
+#
+# Runtime 75 ms Beats 94%
+# Memory 210.9 MB Beats 76%
+def zigzag_level_order(root)
+  return [] if root.nil?
+
+  levels = []
+  queue = [root]
+
+  until queue.empty?
+    current_level = []
+    is_reverse = levels.size.odd?
+
+    queue.size.times do
+      node = queue.shift
+      if is_reverse
+        current_level.unshift(node.val)
+      else
+        current_level << node.val
+      end
+      queue << node.left if node.left
+      queue << node.right if node.right
+    end
+
+    levels << current_level
+  end
+
+  levels
+end
+
 # Runtime 105 ms Beats 82%
 # Memory 211 MB Beats 50%
-def zigzag_level_order(root)
+def zigzag_level_order_myfirst(root)
   return [] if root.nil?
 
   levels = []
