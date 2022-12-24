9bdb38cebc7295e2efe38a900bae76745ebbc2c1
akahige <akahigeg@gmail.com>
2022-12-24 13:31:00 +0900

    コメント追加

--- fibonacci/main.rb
+++ fibonacci/main.rb
@@ -1,18 +1,25 @@
 # ダイナミックプログラミング
 
 # フィボナッチ数列を普通に出力
+# 0から
+#
+# 再帰で書くと速度が問題になる以前にstack level too deepになってしまうのでループで
+#
 def fib(n)
   nums = []
+
   (0..n).each do |i|
     if i == 0
       nums << 0
       next
     end
+
     if i == 1
       nums << 1
       next
     end
-    nums << nums[i - 2] + nums[i - 1]
+
+    nums << nums[i - 2] + nums[i - 1] # すでにメモ化
   end
 
   nums
@@ -24,4 +31,17 @@ p fib(2)
 p fib(3)
 p fib(4)
 p fib(10)
-p fib(100)
+p fib(30)
+
+def fib_last(n)
+  fib(n).last
+end
+
+p fib_last(0)
+p fib_last(1)
+p fib_last(2)
+p fib_last(3)
+p fib_last(4)
+p fib_last(10)
+p fib_last(30)
+# p fib_last(100000)
