53fccb5c3de4ad74c48939c1919baca0445ee657
akahige <akahigeg@gmail.com>
2022-12-24 14:13:33 +0900

    再帰とメモ化再帰も実装してみた

--- fibonacci/main.rb
+++ fibonacci/main.rb
@@ -1,11 +1,13 @@
 # ダイナミックプログラミング
 
+require "benchmark"
+
 # フィボナッチ数列を普通に出力
 # 0から
 #
 # 再帰で書くと速度が問題になる以前にstack level too deepになってしまうのでループで
 #
-def fib(n)
+def fib_array(n)
   nums = []
 
   (0..n).each do |i|
@@ -19,10 +21,22 @@ def fib(n)
       next
     end
 
-    nums << nums[i - 2] + nums[i - 1] # すでにメモ化
+    nums << nums[i - 2] + nums[i - 1] # ダイナミックプログラミング
   end
 
   nums
+end
+
+p fib_array(0)
+p fib_array(1)
+p fib_array(2)
+p fib_array(3)
+p fib_array(4)
+p fib_array(10)
+p fib_array(30)
+
+def fib(n)
+  fib_array(n).last
 end
 
 p fib(0)
@@ -32,16 +46,111 @@ p fib(3)
 p fib(4)
 p fib(10)
 p fib(30)
+# p fib_last(100000)
 
-def fib_last(n)
-  fib(n).last
+# ダイナミックプログラミングではないパターン
+# メモリが節約できるはず
+def fib_no_dp(n)
+  sum = 0
+  prev = 0
+  prev_prev = 0
+  (0..n).each do |i|
+    if i == 0
+      sum += 0
+      next
+    end
+
+    if i == 1
+      sum += 1
+      prev = sum
+      next
+    end
+
+    sum = prev + prev_prev
+
+    prev_prev = prev
+    prev = sum
+  end
+
+  sum
 end
 
-p fib_last(0)
-p fib_last(1)
-p fib_last(2)
-p fib_last(3)
-p fib_last(4)
-p fib_last(10)
-p fib_last(30)
-# p fib_last(100000)
+p fib_no_dp(0)
+p fib_no_dp(1)
+p fib_no_dp(2)
+p fib_no_dp(3)
+p fib_no_dp(4)
+p fib_no_dp(10)
+p fib_no_dp(30)
+
+# 再帰
+def fib_recurse(n)
+  return 0 if n == 0
+  return 1 if n == 1
+  fib_recurse(n - 2) + fib_recurse(n - 1)
+end
+
+p fib_recurse(10)
+
+# 再帰メモ化
+@memo = []
+
+def fib_recurse_memo(n, memo = [0, 1])
+  return memo[n] if memo[n]
+  # return 0 if n == 0
+  # return 1 if n == 1
+  memo[n] = fib_recurse_memo(n - 2, memo) + fib_recurse_memo(n - 1, memo)
+end
+
+p fib_recurse_memo(10)
+
+Benchmark.bm 10 do |r|
+  r.report "dp" do
+    10.times { fib(35) }
+  end
+  r.report "no dp" do
+    10.times { fib_no_dp(35) }
+  end
+  r.report "no dp 2" do
+    10.times { fib_no_dp(35) }
+  end
+  r.report "dp 2" do
+    10.times { fib(35) }
+  end
+  r.report "recurse" do
+    # 遅い
+    10.times { fib_recurse(35) }
+  end
+  r.report "recurse with memo" do
+    # 遅い
+    10.times { fib_recurse_memo(35) }
+  end
+end
+
+# require "memprof2"
+
+p "fib"
+result = Benchmark.realtime do
+  100.times { fib(35) }
+end
+p result
+
+p "fib_no_db"
+result = Benchmark.realtime do
+  100.times { fib_no_dp(35) }
+end
+p result
+
+p "fib_recurse"
+result = Benchmark.realtime do
+  # 遅い
+  100.times { fib_recurse(35) }
+end
+p result
+
+p "fib_recurse_memo"
+result = Benchmark.realtime do
+  # 遅い
+  100.times { fib_recurse_memo(35) }
+end
+p result
