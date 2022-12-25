80922e1e5de0a4270d6c7d3739f1628a2a56596b
akahige <akahigeg@gmail.com>
2022-12-25 10:20:16 +0900

    Heapに関するコメント追加

--- meeting-rooms-2/main.rb
+++ meeting-rooms-2/main.rb
@@ -10,6 +10,7 @@ def min_meeting_rooms(intervals)
 
   i = 0
   while i < intervals.size
+    # Heap使わないでいけてるけど効率は悪いか
     rooms.each_with_index do |room, j|
       if room[1] <= intervals[i][0]
         rooms.delete_at(j)
@@ -22,6 +23,30 @@ def min_meeting_rooms(intervals)
   end
 
   rooms.size
+end
+
+# pointerを使う
+# Runtime 90 ms Beats 97.44%
+# Memory 212.1 MB Beats 64.10%
+def min_meeting_rooms_with_pointer(intervals)
+  intervals.sort! { |i1, i2| i1[0] <=> i2[0] }
+  start_times = intervals.map { |i| i[0] }.sort
+  end_times = intervals.map { |i| i[1] }.sort
+
+  s_pointer = 0
+  e_pointer = 0
+  count = 0
+
+  while s_pointer < intervals.size
+    if start_times[s_pointer] >= end_times[e_pointer]
+      e_pointer += 1
+      count -= 1
+    end
+    s_pointer += 1
+    count += 1
+  end
+
+  count
 end
 
 p min_meeting_rooms([[0, 30], [5, 10], [15, 20]])
