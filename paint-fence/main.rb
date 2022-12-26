ed3190315f68a9d9fc9fb647100737d46937be09
akahige <akahigeg@gmail.com>
2022-12-26 14:02:15 +0900

    Ruby 276. Paint Fence Solutionsを参考に抽象化＆メモ化

--- paint-fence/main.rb
+++ paint-fence/main.rb
@@ -2,6 +2,22 @@
 # @param {Integer} k
 # @return {Integer}
 
+# Runtime 83 ms Beats 100%
+# Memory 211 MB Beats 100%
+def num_ways(n, k)
+  @memo = {}
+  total_ways(n, k)
+end
+
+def total_ways(i, color_count)
+  return color_count if i == 1
+  return color_count * color_count if i == 2
+
+  return @memo[i] if @memo[i]
+
+  @memo[i] = (color_count - 1) * (total_ways(i - 1, color_count) + total_ways(i - 2, color_count))
+end
+
 # TLE
 def my_num_ways(n, k)
   @post_count = n
