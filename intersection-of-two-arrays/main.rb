f26887682fd1b0d5bcf2e5d5724e440ae4b11d50
akahige <akahigeg@gmail.com>
2023-01-02 18:33:56 +0900

    349. Intersection of Two Arrays Setを使って実装してみたもの

--- intersection-of-two-arrays/main.rb
+++ intersection-of-two-arrays/main.rb
@@ -2,10 +2,29 @@
 # @param {Integer[]} nums2
 # @return {Integer[]}
 
+require "set"
+
 # Runtime 95 ms Beats 81.36%
 # Memory 211.4 MB Beats 33.90%
-def intersection(nums1, nums2)
+def my_intersection(nums1, nums2)
   nums1.intersection(nums2)
+end
+
+# Runtime 100 ms Beats 76.27%
+# Memory 211.5 MB Beats 15.25%
+def intersection(nums1, nums2)
+  s1 = Set.new(nums1)
+  s2 = Set.new(nums2)
+
+  if s1.size > s2.size
+    set_intersection(s1, s2)
+  else
+    set_intersection(s2, s1)
+  end
+end
+
+def set_intersection(s1, s2)
+  s1.delete_if { |n| !s2.include?(n) }.to_a
 end
 
 p intersection([1, 2, 2, 1], [2, 2])
