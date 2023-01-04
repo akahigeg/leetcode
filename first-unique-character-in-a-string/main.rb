628934dac2a2b04f58bd27897f349448d4a84446
akahige <akahigeg@gmail.com>
2023-01-04 12:21:22 +0900

    Ruby 387. First Unique Character in a String whileでパフォーマンスを上げ、ついでに変数名が実態と違っていたのでリファクタ

--- first-unique-character-in-a-string/main.rb
+++ first-unique-character-in-a-string/main.rb
@@ -3,17 +3,34 @@
 
 # Runtime: 678 ms, faster than 50.00% of Ruby online submissions for First Unique Character in a String.
 # Memory Usage: 217 MB, less than 45.35% of Ruby online submissions for First Unique Character in a String.
+#
+# whileにしたバージョンのパフォーマンス
+# Runtime: 364 ms, faster than 80.23% of Ruby online submissions for First Unique Character in a String.
+# Memory Usage: 212.3 MB, less than 81.40% of Ruby online submissions for First Unique Character in a String.
 def first_uniq_char(s)
-  counts = {}
-  s.chars.each_with_index do |c, i|
-    unless counts[c]
-      counts[c] = i
+  uniq_positions = {}
+
+  ## Array::each is more readable than while
+  # s.chars.each_with_index do |c, i|
+  #   unless uniq_positions[c]
+  #     uniq_positions[c] = i
+  #   else
+  #     uniq_positions[c] = Float::INFINITY
+  #   end
+  # end
+
+  # while is typically faster than Array::each
+  i = 0
+  while i < s.size
+    unless uniq_positions[s[i]]
+      uniq_positions[s[i]] = i
     else
-      counts[c] = Float::INFINITY
+      uniq_positions[s[i]] = Float::INFINITY
     end
+    i += 1
   end
 
-  first_uniq_position = counts.values.min
+  first_uniq_position = uniq_positions.values.min
 
   first_uniq_position == Float::INFINITY ? -1 : first_uniq_position
 end
