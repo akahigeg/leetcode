da7cbfbb79014837d3d2f41cb76eb1f2b4bb4533
akahige <akahigeg@gmail.com>
2023-01-04 12:01:27 +0900

    Ruby 387. First Unique Character in a String 分かりやすいコードになるようにリファクタ

--- first-unique-character-in-a-string/main.rb
+++ first-unique-character-in-a-string/main.rb
@@ -1,11 +1,11 @@
 # @param {String} s
 # @return {Integer}
 
-# Runtime: 1015 ms, faster than 26.74% of Ruby online submissions for First Unique Character in a String.
-# Memory Usage: 217.5 MB, less than 11.63% of Ruby online submissions for First Unique Character in a String.
+# Runtime: 678 ms, faster than 50.00% of Ruby online submissions for First Unique Character in a String.
+# Memory Usage: 217 MB, less than 45.35% of Ruby online submissions for First Unique Character in a String.
 def first_uniq_char(s)
   counts = {}
-  s.split(//).each_with_index do |c, i|
+  s.chars.each_with_index do |c, i|
     unless counts[c]
       counts[c] = i
     else
@@ -13,7 +13,7 @@ def first_uniq_char(s)
     end
   end
 
-  first_uniq_position = counts.map { |_, i| i }.min
+  first_uniq_position = counts.values.min
 
   first_uniq_position == Float::INFINITY ? -1 : first_uniq_position
 end
