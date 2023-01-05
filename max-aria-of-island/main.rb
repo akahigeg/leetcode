0f821c888d4e2c3321c293609109267228dbe42c
akahige <akahigeg@gmail.com>
2023-01-05 11:33:39 +0900

    Ruby 395. Max Area of Islands 端と端がつながってしまう問題はあるが広さを出すことはできるようになった

--- max-aria-of-island/main.rb
+++ max-aria-of-island/main.rb
@@ -1,20 +1,46 @@
 # @param {Integer[][]} grid
 # @return {Integer}
 def max_area_of_island(grid)
-  p "size: #{grid[0].size} * #{grid.size}"
+  max_area = 0
+  stack = []
 
   y = 0
   while y < grid.size
     x = 0
-    line = []
     while x < grid[0].size
-      line << grid[y][x]
+      current_area = search_island([y, x], 0, grid)
+
+      max_area = [max_area, current_area].max
       x += 1
     end
-    p line
     y += 1
   end
+
+  max_area
 end
 
-p max_area_of_island([[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]) #=> 6
-p max_area_of_island([[0, 0, 0, 0, 0, 0, 0, 0]]) #=> 0
+def search_island(from, current_area, grid)
+  y, x = from
+  return current_area if grid[y].nil? || grid[y][x].nil? || grid[y][x] != 1
+
+  grid[y][x] = 0
+  current_area += 1
+
+  [[y + 1, x], [y - 1, x], [y, x + 1], [y, x - 1]].each do |yx|
+    current_area = search_island(yx, current_area, grid) unless yx[0].negative? && yx[1].negative?
+  end
+
+  current_area
+end
+
+p max_area_of_island([[1, 0, 0, 0, 0, 0, 0, 0]]) #=> 0
+p max_area_of_island([[1, 1, 0, 0, 1, 0, 0, 0]]) #=> 0
+p max_area_of_island([[1, 1, 0, 0, 1, 1, 1, 0]]) #=> 0
+p max_area_of_island([
+    [1, 1, 0, 0, 1, 1, 1, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 1, 1, 1, 0],
+  ]) #=> 0
+
+# p max_area_of_island([[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]) #=> 6
+# p max_area_of_island([[0, 0, 0, 0, 0, 0, 0, 0]]) #=> 0
