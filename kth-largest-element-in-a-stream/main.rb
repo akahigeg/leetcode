7e05bb99ec42130444abaa7e688768b4643e4d49
akahige <akahigeg@gmail.com>
2023-01-05 22:53:50 +0900

    Kth Largest Element in a Stream Heapを使わない解法

--- kth-largest-element-in-a-stream/main.rb
+++ kth-largest-element-in-a-stream/main.rb
@@ -1,20 +1,23 @@
-# Time Limit Exceeded
+# ref: https://leetcode.com/problems/kth-largest-element-in-a-stream/discuss/1172853/Ruby-Faster-100
 class KthLargest
 
   # :type k: Integer
   # :type nums: Integer[]
   def initialize(k, nums)
-    @k = k
-    @nums = nums.sort
+    @max_size = k
+    @data = nums.sort.reverse[0...k]
   end
 
   # :type val: Integer
   # :rtype: Integer
   def add(val)
-    # (@nums + [val]).sort[@k - 1]
-    @nums << val
-    @nums.sort! { |n1, n2| n2 <=> n1 }
-    @nums[@k - 1]
+    if (i = @data.bsearch_index { |x| x < val })
+      @data.insert(i, val)
+    else
+      @data.push(val)
+    end
+    @data.pop if @data.length > @max_size
+    @data.last
   end
 end
 
@@ -22,8 +25,14 @@ end
 # obj = KthLargest.new(k, nums)
 # param_1 = obj.add(val)
 obj = KthLargest.new(3, [4, 5, 8, 2])
-p obj.add(3)
-p obj.add(5)
-p obj.add(10)
-p obj.add(9)
-p obj.add(4)
+p obj.add(3) #=> 4 [8, 5, 4, 3, 2]
+p obj.add(5) #=> 5 [8, 5, 5, 4, 3, 2]
+p obj.add(10) #=> 5
+p obj.add(9) #=> 8
+p obj.add(4) #=> 8
+
+obj = KthLargest.new(1, [])
+p obj.add(-3)
+
+obj = KthLargest.new(2, [0])
+p obj.add(-1)
