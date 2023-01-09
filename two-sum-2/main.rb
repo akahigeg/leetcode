2e043e5d6f1f886363f0fbd369920102a98d02d9
akahige <akahigeg@gmail.com>
2023-01-09 11:05:52 +0900

    Ruby 167. Two Sum II - Input Array Is Sorted ブルートフォースで実装

---
+++ two-sum-2/main.rb
@@ -0,0 +1,19 @@
+# @param {Integer[]} numbers
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(numbers, target)
+  i = 0
+  while i < numbers.size - 1
+    j = i + 1
+    while j < numbers.size
+      return [i, j] if numbers[i] + numbers[j] == target
+      j += 1
+    end
+    i += 1
+  end
+end
+
+p two_sum([2, 7, 11, 15], 9)
+p two_sum([2, 3, 4], 6)
+p two_sum([1, 2, 3, 4], 6)
+p two_sum([-1, 0], -1)
