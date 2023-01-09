652446522f5f8d412822356de326b7b29a85aef3
akahige <akahigeg@gmail.com>
2023-01-09 11:46:47 +0900

    Ruby 167. Two Sum II - Input Array Is Sorted 2ポインターで解いた

--- two-sum-2/main.rb
+++ two-sum-2/main.rb
@@ -2,9 +2,29 @@
 # @param {Integer} target
 # @return {Integer[]}
 
+# ref: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/solution/
+# Runtime: 82 ms, faster than 95.00% of Ruby online submissions for Two Sum II - Input Array Is Sorted.
+# Memory Usage: 212.3 MB, less than 10.00% of Ruby online submissions for Two Sum II - Input Array Is Sorted.
+def two_sum(numbers, target)
+  lo = 0
+  hi = numbers.size - 1
+  while lo < hi
+    sum = numbers[lo] + numbers[hi]
+    return [lo + 1, hi + 1] if sum == target
+
+    if sum < target
+      lo += 1
+    else
+      hi -= 1
+    end
+  end
+
+  [-1, -1]
+end
+
 # Runtime: 119 ms, faster than 65.00% of Ruby online submissions for Two Sum II - Input Array Is Sorted.
 # Memory Usage: 212.3 MB, less than 7.14% of Ruby online submissions for Two Sum II - Input Array Is Sorted.
-def two_sum(numbers, target)
+def my_two_sum(numbers, target)
   i = 0
   while i < numbers.size - 1
     j = numbers[(i + 1)..].bsearch_index { |n| n + numbers[i] >= target }
