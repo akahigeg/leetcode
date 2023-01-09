52f8136350e7fc938ad195a6879da20eefdd7ccc
akahige <akahigeg@gmail.com>
2023-01-09 11:42:40 +0900

    Ruby 167. Two Sum II - Input Array Is Sorted 二分探索を使って解いたけどスペースがO(1)になってないかも？

--- two-sum-2/main.rb
+++ two-sum-2/main.rb
@@ -1,16 +1,24 @@
 # @param {Integer[]} numbers
 # @param {Integer} target
 # @return {Integer[]}
+
+# Runtime: 119 ms, faster than 65.00% of Ruby online submissions for Two Sum II - Input Array Is Sorted.
+# Memory Usage: 212.3 MB, less than 7.14% of Ruby online submissions for Two Sum II - Input Array Is Sorted.
 def two_sum(numbers, target)
   i = 0
   while i < numbers.size - 1
-    j = i + 1
-    while j < numbers.size
-      # 1-indexedなのでそれぞれ+1して返す
-      return [i + 1, j + 1] if numbers[i] + numbers[j] == target
-      j += 1
+    j = numbers[(i + 1)..].bsearch_index { |n| n + numbers[i] >= target }
+
+    if j.nil? || numbers[i] + numbers[j + i + 1] != target
+      i += 1
+      next
     end
-    i += 1
+
+    # p "nums[i]: #{numbers[i]}, target: #{target}, #{target - numbers[i]}"
+    # 1-indexedなのでそれぞれ+1して返す
+    one_indexed_i = i + 1
+    one_indexed_j = j + one_indexed_i + 1
+    return [one_indexed_i, one_indexed_j]
   end
 end
 
@@ -18,3 +26,5 @@ p two_sum([2, 7, 11, 15], 9)
 p two_sum([2, 3, 4], 6)
 p two_sum([1, 2, 3, 4], 6)
 p two_sum([-1, 0], -1)
+p two_sum([5, 25, 75], 100)
+p two_sum([3, 24, 50, 79, 88, 150, 345], 200)
