cfc6a71b7e743dde6fa1d3aae0bb87068d36e6a5
akahige <akahigeg@gmail.com>
2023-01-17 10:10:09 +0900

    Ruby 77. Combinations 仮実装

---
+++ combinations/main.rb
@@ -0,0 +1,21 @@
+# @param {Integer} n
+# @param {Integer} k
+# @return {Integer[][]}
+def combine(n, k)
+  result = []
+
+  i = 1
+  while i <= n
+    j = i + 1
+    while j <= n
+      result << [i, j]
+      j += 1
+    end
+    i += 1
+  end
+
+  result
+end
+
+p combine(4, 2)
+p combine(1, 1)
