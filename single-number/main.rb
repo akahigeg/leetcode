af7bc84dffb4eb81e54568baf81f6dbd64cf1c91
akahige <akahigeg@gmail.com>
2023-01-20 11:01:34 +0900

    Ruby 136. Single Number バリエーション

--- single-number/main.rb
+++ single-number/main.rb
@@ -12,6 +12,21 @@ def single_number(nums)
   end
 end
 
+# Runtime: 98 ms, faster than 76.16% of Ruby online submissions for Single Number.
+# Memory Usage: 213.2 MB, less than 52.33% of Ruby online submissions for Single Number.
+def single_number_with_mash(nums)
+  nums.uniq.sum * 2 - nums.sum
+end
+
+def single_number_with_bit_manupilation(nums)
+  a = 0
+  nums.each do |i|
+    a ^= i
+  end
+
+  a
+end
+
 p single_number([2, 2, 1])
 p single_number([4, 1, 2, 2, 1])
 p single_number([1])
