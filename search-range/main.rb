873e764948d0740de6d4ce6fc89f504a89a5af76
akahige <akahigeg@gmail.com>
2023-02-23 15:25:45 +0900

    Ruby 34. Find First and Last Position of Element in Sorted Array ワンライナー

--- search-range/main.rb
+++ search-range/main.rb
@@ -14,6 +14,15 @@ def search_range(nums, target)
   last_index = nums.size - 1 - nums.reverse.map { |n| -n }.bsearch_index { |n| n >= -target }
 
   [first_index, last_index]
+
+  # [nums.index(target) || -1, nums.rindex(target) || -1]
+end
+
+# Runtime: 79 ms, faster than 85.90% of Ruby online submissions for Find First and Last Position of Element in Sorted Array.
+# Memory Usage: 211.6 MB, less than 66.67% of Ruby online submissions for Find First and Last Position of Element in Sorted Array.
+def search_range_one_liner(nums, tareget)
+  # Array.indexおよびrindexは要素数が少なければ線形探索、多ければ二分探索を行う
+  [nums.index(target) || -1, nums.rindex(target) || -1]
 end
 
 p search_range([5, 7, 7, 8, 8, 10], 8)
