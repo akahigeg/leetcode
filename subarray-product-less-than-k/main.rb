40d3881621747f696b0ba2dc1f270d751ef300dd
akahige <akahigeg@gmail.com>
2023-03-04 15:40:08 +0900

    Ruby 713. Subarray Product Less Than K (Sliding Window)

--- subarray-product-less-than-k/main.rb
+++ subarray-product-less-than-k/main.rb
@@ -2,17 +2,42 @@
 # @param {Integer} k
 # @return {Integer}
 
-# TLE
+# ref: https://leetcode.com/problems/subarray-product-less-than-k/solution/
+# Runtime: 189 ms, faster than 47.06% of Ruby online submissions for Subarray Product Less Than K.
+# Memory Usage: 217.2 MB, less than 82.35% of Ruby online submissions for Subarray Product Less Than K.
 def num_subarray_product_less_than_k(nums, k)
+  return 0 if k <= 1
+
+  product = 1
+  count = 0
+  l = 0
+
+  nums.each_with_index do |n, r|
+    product *= n
+    while product >= k
+      product /= nums[l]
+      l += 1
+    end
+
+    count += r - l + 1
+  end
+
+  count
+end
+
+# TLE
+def my_num_subarray_product_less_than_k(nums, k)
   subarrays = []
+  count = 0
   l = 0
   r = 0
 
   subarray = []
   while l < nums.size
+    # カウントだけ返せばいいのに実際に配列を作っているので重い
     subarray << nums[r]
     if nums[r] && subarray.reduce(:*) < k
-      subarrays.push(subarray.clone)
+      count += 1
       r += 1
     else
       subarray = []
@@ -21,7 +46,7 @@ def num_subarray_product_less_than_k(nums, k)
     end
   end
 
-  subarrays.count
+  count
 end
 
 p num_subarray_product_less_than_k([10, 5, 2, 6], 100) # => 8
